d09a3de5a7eb6c2fa68caabaf3abf3d0c6871185
Narayan Chouhan <24je0661@iitism.ac.in>
2024-10-24 10:01:05 +0530

    Square and Cube implemented

--- calculator.c
+++ calculator.c
@@ -1,88 +1,24 @@
 #include <stdio.h>
 
-int add(int i, int j) {
-    return (i + j);
+// Function to calculate square
+double square(double num) {
+    return num * num;
 }
 
-int subtract(int i, int j) {
-    return (i - j);
-}
-
-int multiply(int i, int j) {
-    return (i * j);
-}
-
-float divide(int i, int j) {
-    if (j == 0) {
-        printf("Error: Division by zero is not allowed.\n");
-        return 0; // Return 0 to indicate an error.
-    }
-    return ((float) i / j);  // Explicit type casting to float for decimal results.
+// Function to calculate cube
+double cube(double num) {
+    return num * num * num;
 }
 
 int main() {
-    int a, b;
-    char operation;
-    char confirm = 'n';
+    double number;
 
-    do {
-        printf("\nChoose an operation (+, -, *, /, ^, #, i):\n");
-        printf("+: Addition\n");
-        printf("-: Subtraction\n");
-        printf("*: Multiplication\n");
-        printf("/: Division\n");
-        printf("^: Square of a number\n");
-        printf("#: Cube of a number\n");
-        printf("i: Inverse of a number\n");
-        
-        printf("Enter operation: ");
-        scanf(" %c", &operation);
+    printf("Enter a number: ");
+    scanf("%lf", &number);
 
-        switch (operation) {
-            case '+':
-                printf("Enter two integers: ");
-                scanf("%d %d", &a, &b);
-                printf("The result of %d + %d is %d\n", a, b, add(a, b));
-                break;
-            case '-':
-                printf("Enter two integers: ");
-                scanf("%d %d", &a, &b);
-                printf("The result of %d - %d is %d\n", a, b, subtract(a, b));
-                break;
-            case '*':
-                printf("Enter two integers: ");
-                scanf("%d %d", &a, &b);
-                printf("The result of %d * %d is %d\n", a, b, multiply(a, b));
-                break;
-            case '/':
-                printf("Enter two integers: ");
-                scanf("%d %d", &a, &b);
-                if (b != 0) {
-                    printf("The result of %d / %d is %.2f\n", a, b, divide(a, b));
-                }
-                break;
-            case '^':
-                printf("Enter an integer: ");
-                scanf("%d", &a);
-                printf("Will be implemented soon...\n");
-                break;
-            case '#':
-                printf("Enter an integer: ");
-                scanf("%d", &a);
-                printf("Will be implemented soon...\n");
-                break;
-            case 'i':
-                printf("Enter an integer: ");
-                scanf("%d", &a);
-                printf("Will be implemented soon...\n");
-                break;
-            default:
-                printf("Invalid operation. Please try again.\n");
-        }
-
-        printf("Do you want to perform another operation [y/N]? ");
-        scanf(" %c", &confirm);
-    } while (confirm == 'y' || confirm == 'Y');
+    printf("Square of %.2lf is %.2lf\n", number, square(number));
+    printf("Cube of %.2lf is %.2lf\n", number, cube(number));
 
     return 0;
 }
+
